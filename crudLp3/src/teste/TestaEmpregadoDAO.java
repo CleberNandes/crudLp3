61f79bb2285766e9ca1c7e5747a2d12603de661c
1600763 <1600763@Sala31224.fit.net>
2017-03-10 11:48:00 -0300

    Primeiro commit java

---
+++ crudLp3/src/teste/TestaEmpregadoDAO.java
@@ -0,0 +1,34 @@
+package teste;
+
+import modelo.Empregado;
+import modelo.EmpregadoDAO;
+
+public class TestaEmpregadoDAO {
+	public static void main(String args[]){
+		/*Empregado empregado = new Empregado();
+		empregado.setNome("Luiz");
+		empregado.setIdade(45);
+		EmpregadoDAO.grava(empregado);*/
+		
+		// testa alteração
+		
+		/*Empregado empregado = new Empregado();
+		empregado.setId(2);
+		empregado.setNome("Luiz");
+		empregado.setAge(45);
+		EmpregadoDAO.altera(empregado);*/
+		
+		// testa exclusão
+		
+		//EmpregadoDAO.exclui(1);
+		
+		// lista empregados
+		
+		//EmpregadoDAO.lista();
+		
+		// le um único empregado
+		
+		//EmpregadoDAO.le(3);
+	}
+	
+}
